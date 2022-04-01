438cc429ca068aaad16a6c46bad73901ab433c03
Vijay Singh <vijaymanral5@gmail.com>
2022-04-01 13:50:18 +0530

    Update Main.go

--- Main.go
+++ Main.go
@@ -7,12 +7,43 @@ import (
 	"hello"
 )
 
-// PACKAGE LEVE VARIABLE
+// PACKAGE LEVE VARIABLE (calmel case)
 var globalVariable int = 8784524
 
 func main() {
 	// BEFORE RUNNING THIS PLEASE READ README.MD FILE
 
+	initializeVariable()
+	howToCallGlobalVariable()
+	howToCallPackageLevelVariable()
+	howToCallOtherPackageMethod()
+
+}
+
+func howToCallPackageLevelVariable() {
+	//==========CALLING PACKAGE LEVE VARIABLE ==================
+	//Calling Hello method from greetings folder
+	fmt.Println("====={CALLING Global LEVE VARIABLE}=====")
+	fmt.Println(greetings.GlobalVariable)
+}
+
+func howToCallOtherPackageMethod() {
+	//==========CALLING METHOD FROM DIFFERENT FILE ==================
+	//Calling Hello method from greetings folder
+	fmt.Println("====={CALLING METHOD FROM DIFFERENT FILE}=====")
+	greetings.Hello("greetings.go greetings method")
+
+	//Calling PrintHello method from hello folder
+	hello.PrintHello()
+}
+
+func howToCallGlobalVariable() {
+	//==========HOW TO CALL GLOBAL VARIABLE ==================
+	fmt.Println("====={GLOBAL VARIABLE}=====")
+	fmt.Println(globalVariable)
+}
+
+func initializeVariable() {
 	//==========HOW TO INITIALIZE ==================
 	// 	LOCAL VARIABLE
 
@@ -22,22 +53,4 @@ func main() {
 	fmt.Println("====={HOW TO INITIALIZE}=====")
 	var myVariable = "Hello, Variables" // Declaration wit Initialization
 	fmt.Println(myVariable)
-
-	//==========HOW TO CALL GLOBAL VARIABLE ==================
-	fmt.Println("====={GLOBAL VARIABLE}=====")
-	fmt.Println(globalVariable)
-
-	//==========CALLING METHOD FROM DIFFERENT FILE ==================
-	//Calling Hello method from greetings folder
-	fmt.Println("====={CALLING METHOD FROM DIFFERENT FILE}=====")
-	greetings.Hello("greetings.go greetings method")
-
-	//Calling PrintHello method from hello folder
-	hello.PrintHello()
-
-	//==========CALLING PACKAGE LEVE VARIABLE ==================
-	//Calling Hello method from greetings folder
-	fmt.Println("====={CALLING Global LEVE VARIABLE}=====")
-	fmt.Println(greetings.GlobalVariable)
-
 }
