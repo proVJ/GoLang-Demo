6803247bc9b67f26e4f1502dbdead3f629bc96cc
Vijay Singh <vijaymanral5@gmail.com>
2022-04-01 13:23:58 +0530

    UPDATE MAIN.GO

--- Main.go
+++ Main.go
@@ -7,17 +7,29 @@ import (
 	"hello"
 )
 
+// GLOBAL VARIABLE
+var globalVariable int = 8784524
+
 func main() {
-	// Before running this Please Read Me File
+	// BEFORE RUNNING THIS PLEASE READ README.MD FILE
+
+	//==========HOW TO INITIALIZE ==================
+	// 	LOCAL VARIABLE
 
 	var intVariable int // Declaration
 	intVariable = 45245 // Initialization
 	fmt.Println(intVariable)
-
+	fmt.Println("====={HOW TO INITIALIZE}=====")
 	var myVariable = "Hello, Variables" // Declaration wit Initialization
 	fmt.Println(myVariable)
 
+	//==========HOW TO CALL GLOBAL VARIABLE ==================
+	fmt.Println("====={GLOBAL VARIABLE}=====")
+	fmt.Println(globalVariable)
+
+	//==========CALLING METHOD FROM DIFFERENT FILE ==================
 	//Calling Hello method from greetings folder
+	fmt.Println("====={CALLING METHOD FROM DIFFERENT FILE}=====")
 	greetings.Hello("greetings.go greetings method")
 
 	//Calling PrintHello method from hello folder
