4ba427e3ee71cda7c29ad33523627c310eb4c555
Vijay Singh <vijaymanral5@gmail.com>
2022-04-01 12:17:28 +0530

    update

--- variables.go
+++ variables.go
@@ -15,9 +15,9 @@ func main() {
 	var myVariable = "Hello, Variables" // Declaration wit Initialization
 	fmt.Println(myVariable)
 
-	//Calling hello method from hello folder
+	//Calling hello method from greetings folder
 	greeting.Hello("greetings.go\\greetings")
 
-	//Calling hello method from greetings folder
+	//Calling hello method from hello
 	hello.PrintHello()
 }
