f7c719f0ba2fb415a76e71a0fb78b46cd23a426b
Vijay Singh <vijaymanral5@gmail.com>
2022-04-01 12:13:53 +0530

    go lang demo
    go lang demo

---
+++ hello/hello.go
@@ -0,0 +1,9 @@
+package hello
+
+import (
+	"fmt"
+)
+
+func PrintHello() {
+	fmt.Println("Hello, Test")
+}
---
+++ variables.go
@@ -0,0 +1,23 @@
+package main
+
+import (
+	"fmt"
+
+	greeting "./greetings"
+	hello "./hello"
+)
+
+func main() {
+	var intVariable int // Declaration
+	intVariable = 45245 // Initialization
+	fmt.Println(intVariable)
+
+	var myVariable = "Hello, Variables" // Declaration wit Initialization
+	fmt.Println(myVariable)
+
+	//Calling hello method from greetings folder
+	greeting.Hello("greetings.go\\greetings")
+
+	//Calling hello method from greetings folder
+	hello.PrintHello()
+}
